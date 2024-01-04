e9af966c01a2ef97ba5f3df449c8fb1145690f9d
Pravallika Venigalla <pravallika.venigalla@amnetdigital.com>
2024-01-04 20:14:12 +0530

    data ingestion added

---
+++ src/cnnClassifier/constants/__init__.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+
+# CONFIG_FILE_PATH = Path("config/config.yaml")
+# PARAMS_FILE_PATH = Path("params.yaml")
+
+
+CONFIG_FILE_PATH = Path(r"C:\Users\pravallika.venigalla\Desktop\Blur-and-Non-Blur-image-classification-Deep-Learning-Project\config\config.yaml")
+PARAMS_FILE_PATH = Path(r"C:\Users\pravallika.venigalla\Desktop\Blur-and-Non-Blur-image-classification-Deep-Learning-Project\params.yaml")
